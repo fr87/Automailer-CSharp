7b5dabeec8e8f40f67e505bbe49c81d9937eb92d
Florian <florian.renner@gmx.de>
2016-07-06 13:58:58 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,11 +11,44 @@ namespace Automailer
             {
                 String Date = DateTime.Today.ToString("dd.MM");
                 string Content = "Der Artikel-Bericht für den " + Date + " wurde erfolgreich erstellt und befindet sich im Anhang.";
+                
+                MailMessage msg = new MailMessage();
+                
+                msg.Attachments.Add(new Attachment(@"C:\..."));
+                
+                SmtpClient client = new SmtpClient();
+                client.Port = 25;
+                client.Host = "smtpserver";
+                client.Timeout = 10000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new System.Net.NetworkCredential("LOGINNAME", "PASSWORD");
+                msg.From = new MailAddress("SENDERADDRESS", "SENDER NAME");
+                msg.To.Add(new MailAddress("RECIPIENT"));
+                msg.Subject = "Subject";
+                msg.IsBodyHtml = true;
+                msg.Body = Content;
+                client.Send(msg);
             }
             
             catch (Exception e)
             {
-                
+                String Date = DateTime.Today.ToString("dd.MM");
+                string Content = "Errormessage";
+                MailMessage errmsg = newMailMessage();
+                SmtpClient client = new SmtpClient();
+                client.Port = 25;
+                client.Host = "smtpserver";
+                client.Timeout = 10000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new System.Net.NetworkCredential("LOGINNAME", "PASSWORD");
+                errmsg.From = new MailAddress("SENDERADDRESS", "SENDER NAME");
+                errmsg.To.Add(new MailAddress("RECIPIENT"));
+                errmsg.Subject = "Error-Subject";
+                errmsg.IsBodyHtml = true;
+                errmsg.Body = Content;
+                client.Send(errmsg);
             }
         }
     }
